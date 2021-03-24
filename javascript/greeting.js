b0b3429c34b74f0d9298a555a83d2ca529987903
alisarf <alisarfaresboulos@gmail.com>
2021-03-24 05:55:45 -0700

    Time Dependent Zone Dependent Greeting
    JS time zone dependent welcome greeting added

--- javascript/greeting.js
+++ javascript/greeting.js
@@ -1,7 +1,27 @@
+//General greeting
 const username = 'Alisar' 
 let message = 'Welcome '
-let greeting = message + username + '!'
+let greeting2 = message + username + '!'
 
 
 var el = document.getElementById('article-1-text')
+el.textContent = greeting2;
+
+
+//Time dependendent Greeting
+let today = new Date();
+let hourNow = today.getHours()
+let greeting;
+
+if (hourNow > 18) {
+    greeting = 'Good Evening ' + username + '!'
+} else if (hourNow > 12) {
+    geeting = 'Good Afternoon ' + username + '!'
+} else if (hourNow > 0 ) {
+    greeting = 'Good Morning ' + username + '!'
+} else {
+    greeting = 'Welcome ' + username + '!'
+}
+
+var el = document.getElementById('article-2-text')
 el.textContent = greeting;
