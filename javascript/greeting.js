ab8f4741e0ba0852087aa4e2703b21f8a29d40d1
alisarf <alisarfaresboulos@gmail.com>
2021-03-25 14:20:00 -0700

    greeting and date
    Time/Date dependent greeting and date added

--- javascript/greeting.js
+++ javascript/greeting.js
@@ -1,27 +1,46 @@
-//General greeting
-const username = 'Alisar' 
+const username = 'Alisar'
+
+/*
+//General greeting 
 let message = 'Welcome '
 let greeting2 = message + username + '!'
 
 
-var el = document.getElementById('article-1-text')
+var el = document.getElementById('greeting2-text')
 el.textContent = greeting2;
+*/
 
 
 //Time dependendent Greeting
 let today = new Date();
-let hourNow = today.getHours()
-let greeting;
+let hourNow = today.getHours();
+let greeting3;
 
 if (hourNow > 18) {
-    greeting = 'Good Evening ' + username + '!'
+    greeting3 = 'Good Evening ' + username + '!';
 } else if (hourNow > 12) {
-    geeting = 'Good Afternoon ' + username + '!'
+    greeting3 = 'Good Afternoon ' + username + '!';
 } else if (hourNow > 0 ) {
-    greeting = 'Good Morning ' + username + '!'
+    greeting3 = `Good Morning ${username}!`;
 } else {
-    greeting = 'Welcome ' + username + '!'
-}
+    greeting3 = 'Welcome ' + username + '!';
+};
 
-var el = document.getElementById('article-2-text')
-el.textContent = greeting;
+let dayNames, day, monthNames, month;
+dayNames = ['Sunday','Monday', 'Tuesday', 'Wednesday' , 'Thursday', 'Friday', 'Saturday'];
+day = dayNames[today.getDay()];
+monthNames = ['January', 'February', 'March', 'April', 'May',
+    'June','July', 'August', 'September', 'October', 'November', 'December']
+month = monthNames[today.getMonth()];
+todayDate = today.getDay()
+
+var el = document.getElementById('greeting-text')
+el.textContent = greeting3;
+
+var el = document.getElementById('dayOfWeek');
+el.innerHTML = '<b>Today is </b>' + day + ' ' + month + ' ' + todayDate + '.';
+
+let year = today.getFullYear()
+
+var el = document.getElementById('copyright');
+el.innerHTML = 'Copyright &copy; ' + year;
